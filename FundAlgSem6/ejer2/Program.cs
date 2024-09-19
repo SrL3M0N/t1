5ea2b2da52f9a590629ae832a1e558f8079f1c6b
SrL3M0N <italocarcheri@gmail.com>
2024-09-19 08:51:27 -0500

    mi cuarto commit

--- FundAlgSem6/ejer2/Program.cs
+++ FundAlgSem6/ejer2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Ingrese ultimo digito de la placa del vehiculo: ");
+String digito = Convert.ToString(Console.ReadLine());
+digito = digito.ToUpper();
+int contaV = 0;
+int contaA = 0;
+int contaR = 0;
+while (digito!="F")
+{
+    int ndigito = Convert.ToInt16(digito);
+    if (ndigito < 4)
+    {
+
+        contaV += 1;
+    }
+    else if (ndigito < 7)
+    {
+
+        contaA += 1;
+    }
+    else if (ndigito < 10) 
+    {
+        contaR += 1;
+    }
+    Console.WriteLine("Ingrese ultimo digito de la placa del vehiculo: ");
+    digito = Convert.ToString(Console.ReadLine());
+    digito = digito.ToUpper();
+
+    
+}
+Console.WriteLine($"han pasado {contaV} con la pegatina verde, {contaA} con la pegatina Azul y {contaR} con la pegatina Roja");
